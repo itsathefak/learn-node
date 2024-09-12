e7cd3bf36bf41c80b79fdf3913784bfa267c5af3
itsathefak <athefayubkhan@gmail.com>
2024-09-12 14:45:13 -0400

    Process and Process.argv

--- backend/script.js
+++ backend/script.js
@@ -3,3 +3,9 @@ let n = 5;
 for (let i = 0; i < 5; i++) {
   console.log("Hello ", i);
 }
+
+let argv = process.argv;
+
+for (let i = 2; i <= argv.length; i++) {
+  console.log("Hello to", argv[i]);
+}
